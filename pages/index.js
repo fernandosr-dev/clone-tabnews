b9d8f461c533f50efa2252f9ec6ecd2da52458b8
Fernando Rodrigues <43592736+fernandosr-dev@users.noreply.github.com>
2025-03-04 00:19:52 +0000

    teste deploy vercel

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>"As melhores oportunidades estão no caminho que niguém quis trilhar antes."</h1>
+    return <h1>"As melhores oportunidades estão no caminho que niguém quis trilhar antes. :)"</h1>
 }
 
 export default Home;
